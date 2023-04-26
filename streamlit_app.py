55ac271b1bbf0c5a708e15287798023cf44f18c7
ParikshatTW <131864113+ParikshatTW@users.noreply.github.com>
2023-04-26 16:13:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣Omega 3 & BlueberryOatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smooothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
