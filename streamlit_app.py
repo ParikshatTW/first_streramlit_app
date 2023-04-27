87f649236a59cbb71335b0a13911f7f8dd7da78b
ParikshatTW <131864113+ParikshatTW@users.noreply.github.com>
2023-04-27 21:18:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-streamlit.dataframe(fruits_to_show)
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------
 
