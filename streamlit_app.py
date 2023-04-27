660c1e0e87c3b432db0bee5ff66bbd2d2a5b1ba3
ParikshatTW <131864113+ParikshatTW@users.noreply.github.com>
2023-04-27 20:35:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
-#import streamlit
-#import pandas
-#import requests
-#import snowflake.connector
+import streamlit
+import pandas
+import requests
+import snowflake.connector
 from urllib.error import URLError
 
 streamlit.title('My Moms New Healthy Diner')
