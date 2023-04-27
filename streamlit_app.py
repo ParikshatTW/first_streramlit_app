f77c42292a4393c21deb086e09fed280eb61b0e6
ParikshatTW <131864113+ParikshatTW@users.noreply.github.com>
2023-04-27 20:49:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,31 +18,37 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), key = ['Avacado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-
-# Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    stremlit.error("Please select a fruit to get information.")
+   else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+    
+except URLError as e:
+  streamlit.error()
+  
+  ------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  streamlit.stop()
+  
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-
-
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
-# write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
 
 streamlit.dataframe(fruityvice_normalized)
-
-streamlit.stop()
-
-
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
