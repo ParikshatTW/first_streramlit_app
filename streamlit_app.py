7f178c23f4923adaeecc71d0efbfdb1ff08a8498
ParikshatTW <131864113+ParikshatTW@users.noreply.github.com>
2023-04-27 20:52:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,16 +39,6 @@ except URLError as e:
   streamlit.error()
   
  #------------------------------------------------------------------------------------------------------------------------------------------------------------
-  streamlit.stop()
-  
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
