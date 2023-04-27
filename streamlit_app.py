4046f23c4bb7395e1d5a8d82d1f5b2d8f9114baa
ParikshatTW <131864113+ParikshatTW@users.noreply.github.com>
2023-04-27 21:30:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,6 @@ try:
  
  #------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-     
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
